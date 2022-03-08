c1a1715316ff2298201c6c6c8b30ecceb1ce10c8
SnowCore8 <snowcore8@gmail.com>
2022-03-08 11:44:08 +0800

    Fix some problems

--- start.sh
+++ start.sh
@@ -10,6 +10,33 @@ clear
 
 clear
 ./mount.sh
+echo "\
+#!/bin/bash
+export PATH=/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+export SHELL=/bin/bash
+export HOME=/root
+export TERM=linux
+apt-get update && apt-get upgrade && apt-get clean
+apt-get install language-pack-zh-hans  language-pack-zh-hans-base -y
+apt-get install xfonts-intl-chinese ttf-wqy-microhei ttf-wqy-zenhei  xfonts-wqy -y
+export LANG="zh_CN.UTF-8"
+export LANGUAGE="zh_CN:zh"
+export LC_MONETARY="zh_CN"
+export LC_PAPER="zh_CN"
+export LC_NAME="zh_CN"
+export LC_ADDRESS="zh_CN"
+export LC_TELEPHONE="zh_CN"
+export LC_MEASUREMENT="zh_CN"
+export LC_IDENTIFICATION="zh_CN"
+export LC_ALL="zh_CN.UTF-8"
+cd ~
+clear
+service ssh restart
+bash -i
+clear&&clear
+service ssh stop
+exit
+" >linux/boot/init.sh
 
 clear
 cp /etc/hosts $linux/etc/hosts
