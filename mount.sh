bcd7433a4e6cbe755135c8f9e0771aeab209436b
SnowCore8 <snowcore8@gmail.com>
2022-03-11 23:44:45 +0800

    Repair the language environment

--- mount.sh
+++ mount.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 linux=/data/linux/linux
 mount -o bind /dev $linux/dev
-mount -o bind /dev/shm $linux/dev/shm
 mount -o bind /dev/pts $linux/dev/pts
 mount -o bind /proc $linux/proc
 mount -o bind /sys $linux/sys
--- start.sh
+++ start.sh
@@ -3,7 +3,6 @@ linux=/data/linux/linux
 
 clear
 [ ! -d $linux/dev ] && mkdir -p $linux/dev
-[ ! -d $linux/dev/shm ] && mkdir -p $linux/dev/shm
 [ ! -d $linux/dev/pts ] && mkdir -p $linux/dev/pts
 [ ! -d $linux/proc ] && mkdir -p $linux/proc
 [ ! -d $linux/sys ] && mkdir -p $linux/sys
@@ -12,22 +11,29 @@ clear
 ./mount.sh
 echo "\
 #!/bin/bash
-export PATH=/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin:/usr/local/sbin
-export SHELL=/bin/bash
-export HOME=/root
-export TERM=linux
-apt-get update && apt-get upgrade && apt-get clean
-apt-get install language-pack-zh-hans  language-pack-zh-hans-base -y
-export LANG="zh_CN.UTF-8"
-export LANGUAGE="zh_CN:zh"
-export LC_MONETARY="zh_CN"
-export LC_PAPER="zh_CN"
-export LC_NAME="zh_CN"
-export LC_ADDRESS="zh_CN"
-export LC_TELEPHONE="zh_CN"
-export LC_MEASUREMENT="zh_CN"
-export LC_IDENTIFICATION="zh_CN"
-export LC_ALL="zh_CN.UTF-8"
+export PATH="/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin"
+export ARCH="arm64"
+export HOME="/root"
+export TERM="linux"
+apt-get update && apt-get upgrade && apt-get install -f && apt-get clean
+apt-get install glibc* -y
+apt-get install bison -y
+apt-get install language-pack-zh-hans* -y
+export LANG=zh_CN.UTF-8
+export LANGUAGE=zh_CN:zh
+export LC_CTYPE="zh_CN.UTF-8"
+export LC_NUMERIC="zh_CN.UTF-8"
+export LC_TIME="zh_CN.UTF-8"
+export LC_COLLATE="zh_CN.UTF-8"
+export LC_MONETARY="zh_CN.UTF-8"
+export LC_MESSAGES="zh_CN.UTF-8"
+export LC_PAPER="zh_CN.UTF-8"
+export LC_NAME="zh_CN.UTF-8"
+export LC_ADDRESS="zh_CN.UTF-8"
+export LC_TELEPHONE="zh_CN.UTF-8"
+export LC_MEASUREMENT="zh_CN.UTF-8"
+export LC_IDENTIFICATION="zh_CN.UTF-8"
+export LC_ALL=zh_CN.UTF-8
 cd ~
 clear
 service ssh restart
@@ -40,8 +46,15 @@ chown root:root $linux/boot/init.sh
 chmod 777 $linux/boot/init.sh
 
 clear
-cp /etc/hosts $linux/etc/hosts
-cp /etc/resolv.conf $linux/etc/resolv.conf
+echo "localhost" > $linux/etc/hostsname
+echo "127.0.0.1 localhost" > $linux/etc/hosts
+echo "\
+nameserver 8.8.4.4
+nameserver 8.8.8.8
+nameserver 180.76.76.76
+nameserver 119.29.29.29
+nameserver 114.114.114.114 
+" >$linux/etc/resolv.conf
 
 clear
 chroot $linux /boot/init.sh
